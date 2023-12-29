23114b7456cb68cb2c4e55cde064ed18a90f2e39
jimin <jimin00study@gmail.com>
2023-12-29 20:13:23 +0900

    12/29 학원 백업

---
+++ js/script.js
@@ -0,0 +1,27 @@
+$(function () {
+  // Portfolio 탭메뉴 변수
+  const $tabMenu = $('.tab-menu > li');
+  const $tabcontent = $('.tab-con');
+
+  // s : Portfolio 탭메뉴 설정
+  // 초기 세팅
+  tabAction(0);
+
+  // 탭메뉴를 클릭했을 때
+  $tabMenu.on('click', function () {
+    let idx = $(this).index();
+
+    tabAction(idx);
+  });
+
+  // 함수 정의
+  function tabAction(index) {
+    $tabMenu.removeClass('on');
+    $tabMenu.eq(index).addClass('on');
+
+    // $tabcontent.hide();
+    $tabcontent.addClass('hide');
+    $tabcontent.eq(index).removeClass('hide');
+  }
+  // e : Portfolio 탭메뉴 설정
+});
